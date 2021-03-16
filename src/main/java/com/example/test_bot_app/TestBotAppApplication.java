9ab97ade960c0e9d40e21e2116fef8815bfb924e
EugeneZakhno <zohan1234567@gmail.com>
2021-03-16 23:02:43 +0300

    init

---
+++ src/main/java/com/example/test_bot_app/TestBotAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_bot_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBotAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestBotAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/test_bot_app/TestBotAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_bot_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBotAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

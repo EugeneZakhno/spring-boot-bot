7a32611b55e096f52f7bcdeb99ea8c8952cebc15
EugeneZakhno <zohan1234567@gmail.com>
2021-03-16 23:39:19 +0300

    added class Bot

--- src/main/java/com/example/test_bot_app/TestBotAppApplication.java
+++ src/main/java/com/example/test_bot_app/TestBotAppApplication.java
@@ -2,12 +2,15 @@ package com.example.test_bot_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.ApiContextInitializer;
 
 @SpringBootApplication
 public class TestBotAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TestBotAppApplication.class, args);
-    }
 
+        ApiContextInitializer.init();
+        SpringApplication.run(TestBotAppApplication.class, args);
+    }
 }
